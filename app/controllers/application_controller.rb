37248cd45bebce65d453251950a80726586d42a7
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-10-05 14:08:16 -0400

    add verify_authentication method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  before_action :verify_authentication
+
+  def verify_authentication
+    render json: { error: "Restricted" }, status: :unauthorized
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seeding the database example from newline:
+# ALPHABET = ('a'..'z').to_a
+# PATTERNS = %w(solid bi-color tabby calico tortoiseshell)
+# COLORS   = %w(white black brown blue ginger)
+#
+# 100.times do
+#   Cat.create!(
+#     name:         ALPHABET.sample(8).join.capitalize,
+#     coat_pattern: PATTERNS.sample,
+#     coat_color:   COLORS.sample
+#   )
+# end

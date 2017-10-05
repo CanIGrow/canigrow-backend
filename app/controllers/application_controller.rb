6f863b8458396d17d24a264d6c92afcd5caa9d31
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-10-05 16:58:38 -0400

    more troubleshooting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  protect_from_forgery with: :exception
+  
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
   private

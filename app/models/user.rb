bdd246b9c8cbe6e06f2c7e78c9513748932b0ad0
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-10-13 16:02:48 -0400

    destroying a user also destroys a user's plots

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,10 +15,12 @@
 #
 
 class User < ApplicationRecord
-  has_many :plots
+  has_many :plots, dependent: :destroy
   has_many :plants, through: :plots
   has_secure_token :api_token
   has_secure_password
+
+  attr_accessor: :activation_token
 
   before_validation :downcase_email
   before_validation :downcase_username
@@ -29,6 +31,8 @@ class User < ApplicationRecord
 
   validates_uniqueness_of :username
 
+  before_create :create_activation_digest
+
 
   private
 
@@ -38,6 +42,11 @@ class User < ApplicationRecord
 
   def downcase_username
     username.downcase if username
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 
 end

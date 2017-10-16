215cbf99db0680f61b5e853487bd03ed683f7e44
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-10-16 11:54:32 -0400

    update user params from last migration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,7 +53,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :bio, :location, :email, :activation_digest)
+    params.require(:user).permit(:username, :password, :bio, :location, :email, :activation_digest, :location_private, :twitter, :facebook)
   end
 
   def get_user

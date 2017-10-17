fdab4b07e9cdbac3fa7b42c3930296e83eea72c0
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-10-17 10:13:14 -0400

    add account activation route to request new activation email

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'password_resets/new'
   get 'password_resets/edit'
-  resources :account_activations, only: [:edit]
+  resources :account_activations, only: [:edit, :create]
   resources :password_resets, only: [:new, :create, :edit, :update]
 
   scope '/api' do

1e1eb458593544cedfbecfb95b69eef4d82a370d
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-10-18 15:12:02 -0400

    fixed stuff broken when coding file uploads

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@
 
 class User < ApplicationRecord
   has_attached_file :avatar, styles: {medium: "300x300>", thumb: "100x100>"}, default_url: "/images/:style/missing.png"
-  validates_attachment :avatar, presence: true,
+  validates_attachment :avatar,
   content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
   size: { in: 0..5.megabytes }
 

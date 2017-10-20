b13e8498a247efb6008fad804cf9a3ce5585b403
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-10-20 17:36:57 -0400

    typo in plant model

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -23,6 +23,6 @@
 
 class Plant < ApplicationRecord
   has_and_belongs_to_many :plots, :through => :plants_plots
- -  has_and_belongs_to_many :users, through: :plots
+  has_and_belongs_to_many :users, through: :plots
   has_many :comments, dependent: :destroy
 end
